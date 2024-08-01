141d3e4896c2a0155c046863033c0b4d319aba00
PrathikshaVS <prathiksha4915@gmail.com>
2024-08-01 12:46:19 +0530

    Angular hands-On4

---
+++ Angular/Employee/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Employee } from './model/Employee';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Employee';
+  employee : Employee;
+   
+constructor(){
+  this.employee = new Employee(); 
+}
+
+
+insertEmployee(data : any){
+  this.employee.empId = data.empId;
+  this.employee.empName = data.empName;
+  this.employee.empSalary = data.empSalary;
+}
+}
